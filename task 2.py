eb98e5857369129635070a979f2197fd61256d38
Vishaldeepsingh1223 <114922130+Vishaldeepsingh1223@users.noreply.github.com>
2022-10-03 13:48:52 +0100

    Delete task 2.py

--- task 2.py
+++
@@ -1,8 +0,0 @@
-Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-print ("This is my first class")
-This is my first class
-print ("I enjoy my first coding ")
-I enjoy my first coding 
-print ("I love leeds beckett university ")
-I love leeds beckett university 
